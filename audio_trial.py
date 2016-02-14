c44ac62840033d3e89cadaabccb31752ccaad9dc
Jack Norman <jwnorman91@gmail.com>
2016-02-14 15:05:32 -0800

    added comments to explain bin-zip-roll-pop; changed bin to bin_itr since bin is a builtin function

--- audio_trial.py
+++ audio_trial.py
@@ -9,9 +9,9 @@ class PiuHash(object):
     def __init__(self, window_length=1, bins=[30,40,80,120,180,300]):
         self.piu_hash = defaultdict(list)
         self.window_length = window_length
-        self.bins = bins
-        self.bins = zip(self.bins, np.roll(self.bins, -1))
-        self.bins.pop()
+        self.bins = bins # ex: [30, 40, 80]
+        self.bins = zip(self.bins, np.roll(self.bins, -1)) # ex: [(30,40), (40,80), (80,30)]
+        self.bins.pop() # ex: [(30,40), (40,80)]
 
     def hash_song(self, channel=None, meta=""):
         """ Hash one song
@@ -19,26 +19,28 @@ class PiuHash(object):
             channel: a list or numpy array of frequencies from a wav file
             meta: unique id of wav file
         """
-
         num_splits = np.ceil(len(channel)/float(44100*self.window_length))
         song_segments = np.array_split(channel, num_splits)
         for i, song_segment in enumerate(song_segments):
             fd = abs(np.fft.fft(song_segment))
             t = np.arange(len(song_segment))
             freq = abs(np.fft.fftfreq(len(fd),1/float(44100)))
-            hash_temp = tuple([self.argmax_frequency(fd, freq, bin) for bin in self.bins])
+            hash_temp = tuple([self.argmax_frequency(fd, freq, bin_itr) for bin_itr in self.bins])
             self.piu_hash[hash_temp].append([i, meta])
 
     @staticmethod
-    def argmax_frequency(fd, freq, bin):
-        relative_argmax = np.argmax(fd[bin[0]:bin[1]])
-        return int(freq[bin[0] + relative_argmax])
+    def argmax_frequency(fd, freq, bin_itr):
+        relative_argmax = np.argmax(fd[bin_itr[0]:bin_itr[1]])
+        return int(freq[bin_itr[0] + relative_argmax])
 
 def main():
     try:
         fs, data = wavfile.read("/Users/gab/Documents/MSAN/ML2/classy.wav")
     except:
-        fs, data = wavfile.read("/Users/jacknorman1/Documents/USF/MSAN/Module3/ML2/Project/classy.wav")
+        try:
+            fs, data = wavfile.read("/Users/jacknorman1/Documents/USF/MSAN/Module3/ML2/Project/classy.wav")
+        except:
+            fs, data = wavfile.read("Bens_path_to_wav_file")
     channel1 = data[:,0]
     meta = 'ARTIST_SONG_ID'
     # plt.plot(channel1)
