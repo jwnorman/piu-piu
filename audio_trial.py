0c54d6005b4fd507f1894c3f9290b2def927082c
Gabrielle Corbett <gab@Gabrielles-MacBook-Pro.local>
2016-02-17 20:35:20 -0800

    comments

--- audio_trial.py
+++ audio_trial.py
@@ -1,3 +1,5 @@
+__author__ = "Gabrielle Corbett, Ben Miroglio, and Jack Norman"
+
 from __future__ import division
 from scipy.io import wavfile
 import numpy as np
@@ -7,7 +9,7 @@ import seaborn
 import operator
 import glob
 import sys
-#%matplotlib inline
+
 
 class PredictSong(object):
     def __init__(self, song, num_predictions):
@@ -20,11 +22,12 @@ class PredictSong(object):
     def song_streamer(song_segments):
         for i, song_segment in enumerate(song_segments):
             fd = abs(np.fft.fft(song_segment))
-            freq = abs(np.fft.fftfreq(len(fd),1/float(44100))) # we may want to consider soft coding this
+            freq = abs(np.fft.fftfreq(len(fd), 1/float(44100)))  # we may want to consider soft coding this
             yield fd, freq
 
+
 class PiuHash(object):
-    def __init__(self, window_length=1, bins=[[30,40,80,120,180,300]]):
+    def __init__(self, window_length=1, bins=[[30, 40, 80, 120, 180, 300]]):
         self.num_hashes = len(bins)
         self.piu_hash = [defaultdict(list) for i in xrange(self.num_hashes)]
         self.window_length = window_length
@@ -40,26 +43,26 @@ class PiuHash(object):
         song_segments = np.array_split(channel, num_splits)
         for i, song_segment in enumerate(song_segments):
             fd = abs(np.fft.fft(song_segment))
-            freq = abs(np.fft.fftfreq(len(fd),1/float(44100))) # we may want to consider soft coding this
+            freq = abs(np.fft.fftfreq(len(fd), 1/float(44100)))  # we may want to consider soft coding this
             self.hash_seg(fd, freq, i, meta=meta)
 
     def hash_dir(self, directory, channel = None, meta=""):
         """ Reads and hashes each .wav song in the directory <dir>
         """
-        files = glob.glob(directory + "/*.wav")
+        files = glob.glob(directory + "*.wav")
         n = len(files)
         i = 1
         for filename in files:
+            print filename
             sys.stdout.write('%.2f%%\r' % (i / n * 100))
             sys.stdout.flush()
             i += 1
             fs, data = wavfile.read(filename)
-            channel1 = data[:,0]
+            channel1 = data[:, 0]
             piu.hash_song(channel1, str(i))
 
-
     def hash_seg(self, fd, freq, i=None, test=False, meta=None):
-        """ Hashes a fourier transformed segment of a song. 
+        """ Hashes a fourier transformed segment of a song 
         """
         ret = list()
         for hash_num in range(self.num_hashes):
@@ -68,14 +71,14 @@ class PiuHash(object):
                 self.piu_hash[hash_num][hash_temp].append([i, meta])
             else: # testing
                 ret.append(hash_temp)
-        if test: return ret
+        if test return ret
 
     def predict(self, song):
         """
         song: the output from wavfile.read()
         """
         pred_song = PredictSong(song, self.num_hashes)
-        channel1 = song[:,0]
+        channel1 = song[:, 0]
         num_splits = np.ceil(len(channel1)/float(44100*self.window_length))
         song_segments = np.array_split(channel1, num_splits)
         streamer = pred_song.song_streamer(song_segments)
@@ -83,10 +86,10 @@ class PiuHash(object):
         match = False
         while not match:
             fd, freq = streamer.next()
-            res = self.hash_seg(fd, freq, test=True) # [(24,48,80,111,200), (11,111,200)]
+            res = self.hash_seg(fd, freq, test=True)  # [(24,48,80,111,200), (11,111,200)]
             for i, key in enumerate(res):
-                pred_song.counters[i] += Counter([elem[1] for elem in self.piu_hash[i][key]]) # running sum
-                pred_song.props[i] = {k: 1/sum(pred_song.counters[i].values()) for k,v in pred_song.counters[i].iteritems()} # proportion        
+                pred_song.counters[i] += Counter([elem[1] for elem in self.piu_hash[i][key]])  # running sum
+                pred_song.props[i] = {k: 1/sum(pred_song.counters[i].values()) for k, v in pred_song.counters[i].iteritems()} # proportion        
                 max_key = max(pred_song.props[i].iteritems(), key=operator.itemgetter(1))[0]
                 if pred_song.props[i][max_key] >= .8:
                     print pred_song.props[i]
@@ -116,6 +119,7 @@ class PiuHash(object):
     def argmax_frequency(fd, freq, bin_itr):
         relative_argmax = np.argmax(fd[bin_itr[0]:bin_itr[1]])
         return int(freq[bin_itr[0] + relative_argmax])
+
 
 def main():
     try:
