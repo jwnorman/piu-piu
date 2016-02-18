d82a7f3a65e7dee8c5012579323db89f5161d655
Ben Miroglio <ben@MB.local>
2016-02-17 17:58:03 -0800

    commenting code

--- grab_wav.py
+++ grab_wav.py
@@ -5,26 +5,40 @@ import shutil
 import uuid
 
 song_lst = {}
-with open('song_meta.csv', 'w') as outfile:
-	outfile.write('id, artist, album, song, format_song')
+with open('song_meta.tsv', 'w') as outfile:
+	#create headers for tsv file
+	outfile.write('\t'.join(['id', 'artist', 'album', 'song', 'format_song']))
 	outfile.write('\n')
+
+	#Grabbing all mp3 files that lie within 'Music' directory
 	for root, dirnames, filenames in os.walk('/Users/ben/Desktop/Orig_songs/Music'): # recursive generator for Music directory
 		for filename in fnmatch.filter(filenames, "*.mp3"):
 			song_path = os.path.join(root, filename)
+
+			#clean the song name to make it programmable
 			new_filename = re.sub(r'^[0-9]+_', '', \
 						   re.sub(r'mp3.*', '.mp3', \
 				           re.sub(r'[(),.-]', '',  \
-				           re.sub(r'\s', '_', filename))))
+				           re.sub(r'\s', '_', filename)))).strip()
 
 			new_path = "/Users/ben/src/msan/adv_machineLearning/music_wav/" + new_filename
-			meta = [uuid.uuid4().hex]#id + \
-			        root.split('/')[-2:]#artist, album + \
-			       [re.sub(r'^[0-9]+', '', filename.strip(".mp3"))]#original song name + \
-			       [new_filename.strip('.mp3')] #formatted song name
-			       
-			outfile.write(','.join(meta))
+
+			#clean the song name but preserve spaces () etc, just get rid of whitespace, .mp3,
+			#and random numbers that are appended to the front of some songs
+			orig = re.sub(r'^[-0-9]+', '', filename.strip(".mp3"))
+
+			#generating unique id for each song with uuid.uuid4()
+			_id = uuid.uuid4().hex]
+			artist, album = root.split('/')[-2:] #arist, album last two levels of root path
+			song = orig.strip() 
+			format_song = new_filename.strip('.mp3') #converting to wav eventually, dont need .mp3 
+
+			meta = [_id, artist, album, song, format_song]
+
+			outfile.write('\t'.join(meta))
 			outfile.write('\n')
 
-			if song_path not in song_lst: # no dups
-				shutil.move(song_path, new_path)
-				song_lst[song_path] = True
+			#code to move files initially, using this script to test formatting
+			# if song_path not in song_lst: # no dups
+			# 	shutil.move(song_path, new_path)
+			# 	song_lst[song_path] = True
