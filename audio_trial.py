e3a3c4553b8d06019d5395ca69a568aaf0fc4a3d
Ben Miroglio <ben@MB.local>
2016-02-18 19:33:19 -0800

    finalized code for progress report

--- audio_trial.py
+++ audio_trial.py
@@ -1,3 +1,9 @@
+"""
+Contains PiuHash and PredictSong classes. 
+Music Recognition Software
+Piu-Piu, Inc. @MSAN-USF
+"""
+
 from __future__ import division
 from scipy.io import wavfile
 import numpy as np
@@ -8,12 +14,15 @@ import operator
 import glob
 import sys
 import fnmatch
-import os, re
+import os
+import re
 import shutil
 import uuid
 import pipes
 import sounddevice as sd
-#%matplotlib inline
+
+__author__ = "Gabby Corbett, Ben Miroglio, Jack Norman"
+
 
 class PredictSong(object):
     def __init__(self, song, piu_hash_obj):
@@ -23,20 +32,30 @@ class PredictSong(object):
         self.props = [Counter() for i in range(piu_hash_obj.num_hashes)]
 
     def song_streamer(self, song_segments):
+        """
+        Takes song_segments and retrieves fd and freq 
+        for one segment at a time (generator)
+        """
         for i, song_segment in enumerate(song_segments):
             yield self.get_fft(song_segment)
-    
+
     @staticmethod
     def get_fft(song_segment):
+        """Performs Fast Fourier Transform on a song segment"""
         fd = abs(np.fft.fft(song_segment))
-        freq = abs(np.fft.fftfreq(len(fd),1/float(44100))) # we may want to consider soft coding this
+        freq = abs(np.fft.fftfreq(len(fd), 1/float(44100)))
         return fd, freq
 
     def predict(self):
         """
-        song: the output from wavfile.read()
+        Segments/hashes the 1st channel in song, 
+        compares to train hashes
         """
-        channel1 = self.song[:,0]
+        try:
+            channel1 = self.song[:,0]
+        except:
+            channel1 = self.song
+
         num_splits = np.ceil(len(channel1)/float(44100*self.piu_hash_obj.window_length))
         song_segments = np.array_split(channel1, num_splits)
         streamer = self.song_streamer(song_segments)
@@ -51,27 +70,22 @@ class PredictSong(object):
             itr_num += 1
     
     def predict_iteration(self, fd, freq, itr_num):
-        res = self.piu_hash_obj.hash_segment(fd, freq, test=True) # [(24,48,80,111,200), (11,111,200)]
-        print 'res: ' + str(res)
-        print 'itr_num: ' + str(itr_num)
+        """
+        Takes fft info and checks train hashes, updating 
+        counter for the test instance
+        """
+        res = self.piu_hash_obj.hash_segment(fd, freq, test=True)
         for i, key in enumerate(res):
-            print 'blah: ' + str(len(self.piu_hash_obj.piu_hash[i][key]))
             if len(self.piu_hash_obj.piu_hash[i][key]) > 0:
-                print '\tblah: ' + str(len(self.piu_hash_obj.piu_hash[i][key]))
-                self.counters[i] += Counter([elem[1] for elem in self.piu_hash_obj.piu_hash[i][key]]) # running sum
-                self.props[i] = {k: self.counters[i][k]/sum(self.counters[i].values()) for k,v in self.counters[i].iteritems()} # proportion
-                print '\t' + 'counters: ' + str(self.counters)
-                print '\t' + 'proportions: ' + str(self.props)
+                self.counters[i] += Counter([elem[1] \
+                                    for elem in self.piu_hash_obj.piu_hash[i][key]]) # running sum
+                self.props[i] = {k: self.counters[i][k]/sum(self.counters[i].values()) \
+                                    for k,v in self.counters[i].iteritems()} # proportion
                 max_key = max(self.props[i].iteritems(), key=operator.itemgetter(1))[0]
-                print ''
                 if (self.props[i][max_key] >= .8) and (itr_num >= 5):
-                    print '\t' + 'success'
-                    print '\t\t' + 'proportions: ' + str(self.props[i])
-                    print '\t\t' + 'max_key: ' + str(max_key)
-                    print ''
                     return max_key
-        #print 'No matches found'
         return False
+
 
 class PiuHash(object):
     def __init__(self, window_length=1, bins=[[30,40,80,120,180,300]]):
@@ -81,45 +95,50 @@ class PiuHash(object):
         self.bins = [self.get_lit(bin_itr) for bin_itr in bins]
         self.meta = {}
 
-    def convert_and_load_songs(self, directory, filter_by= ''):
+    def convert_and_load_songs(self, directory, filter_by=''):
+        """
+        Takes a 'Music' Directory and recursively finds 
+        all files underneath, grabbing all associated 
+        meta data. Converts songs to wav and adds to PiuHash
+        """
         for root, dirnames, filenames in os.walk(directory):
             leaves = fnmatch.filter(filenames, "*" + filter_by)
-            for i, filename in enumerate(leaves): #only look at .mp3s for noe
+            for i, filename in enumerate(leaves):  # only look at .mp3s for noe
                 if re.match('.DS', filename):
                     continue
-                #display progress
+
+                # display progress
                 sys.stdout.write('\r%d' % (i))
                 sys.stdout.flush()
-                #create unique id 
+
+                # create unique id 
                 _id = uuid.uuid4().hex
 
-                song_path = os.path.join(root, filename) #make full accessible path
+                song_path = os.path.join(root, filename)  # make full accessible path
                 formatted_path = os.path.join(root, re.sub(' ', '', filename))
                 shutil.move(song_path, os.path.join(root, formatted_path))
 
                 new_filename = _id + '.wav'
-
                 print 'converting {} to {}'.format(formatted_path, new_filename)
-                #convert to wav and save as <uuid>.wav
+
+                # convert to wav and save as <uuid>.wav
                 os.system('ffmpeg -i {} {}'.format(pipes.quote(formatted_path), new_filename))
-                #clean the song name but preserve spaces () etc, just get rid of whitespace, .mp3,
-                #and random numbers that are appended to the front of some songs
+
+                # clean the song name but preserve spaces () etc, just get rid of whitespace, .mp3,
+                # and random numbers that are appended to the front of some songs
                 orig = re.sub(r'^[-0-9]+', '', filename.strip('.*'))
-                artist, album = root.split('/')[-2:] #arist, album last two levels of root path
-                song = orig.strip() # 'displayable' song #converting to wav eventually, dont need .mp3 
+                artist, album = root.split('/')[-2:]
+                song = orig.strip()
 
                 meta = {'artist': artist, 'album':album,'song': song}
                 self.meta[_id] = meta
+                self.print_hashes()
                 self.hash_song(new_filename, _id, meta)
-                os.remove(new_filename)
-
-
 
     def hash_song(self, filename, uuid, meta=""):
-        """ Hash one song
-
-            channel: a list or numpy array of frequencies from a wav file
-            meta: unique id of wav file
+        """ 
+        Take a path to a file, reads into wav 
+        and adds to hash
         """
         fs, data = wavfile.read(filename)
         try:
@@ -130,13 +149,15 @@ class PiuHash(object):
         song_segments = np.array_split(channel, num_splits)
         for song_segment in song_segments:
             fd = abs(np.fft.fft(song_segment))
-            freq = abs(np.fft.fftfreq(len(fd),1/float(44100))) # we may want to consider soft coding this
+            freq = abs(np.fft.fftfreq(len(fd), 1/float(44100)))
             self.hash_segment(fd, freq, uuid, meta=meta)
 
     def hash_segment(self, fd, freq, uuid=None, test=False, meta=None):
+        """helper function for hash_song"""
         ret = list()
         for hash_num in range(self.num_hashes):
-            hash_temp = tuple([self.argmax_frequency(fd, freq, bin_itr) for bin_itr in self.bins[hash_num]])
+            hash_temp = tuple([self.argmax_frequency(fd, freq, bin_itr) \
+                                for bin_itr in self.bins[hash_num]])
             if not test: # building
                 self.piu_hash[hash_num][hash_temp].append([uuid, meta])
             else: # testing
@@ -144,6 +165,7 @@ class PiuHash(object):
         if test: return ret
 
     def print_hashes(self):
+        """pretty print for hashes"""
         for hash_num in range(self.num_hashes):
             print 'hash ' + str(hash_num) + ' with bins: ' + str(self.bins[hash_num])
             for k,v in self.piu_hash[hash_num].iteritems():
@@ -165,23 +187,14 @@ class PiuHash(object):
         relative_argmax = np.argmax(fd[bin_itr[0]:bin_itr[1]])
         return int(freq[bin_itr[0] + relative_argmax])
 
-def main():
-    try:
-        fs, data = wavfile.read("/Users/gab/Documents/MSAN/ML2/classy.wav")
-    except:
-        try:
-            fs, data = wavfile.read("/Users/jacknorman1/Documents/USF/MSAN/Module3/ML2/Project/el_nino.wav")
-        except:
-            fs, data = wavfile.read("/Users/ben/src/msan/adv_machineLearning/music_wav/el_nino.wav")
-    channel1 = data[:,0]
-    meta = 'ARTIST_SONG_ID'
 
-    piu = PiuHash(bins=[[30,40,80,120,180,300],[0, 100, 200, 300]])
-    piu.hash_song(channel1, meta)
-    piu.print_hashes()
-    pred = PredictSong(data, piu, 10)
-    pred.predict()
+def main():
+    buckets = [[30,40,80,120,180,300],[0, 100, 200, 300], \
+               [0, 350, 3000], np.arange(0, 3000, 100), [0, 200, 1000, 3000], \
+               [300, 1000, 3000], [0, 300, 500, 1000, 2000, 3000], \
+               [0, 100, 200, 400, 800, 1600, 3000]]
+    piu = PiuHash(bins=buckets)
+    piu.convert_and_load_songs('/Users/ben/Desktop/Orig_songs/Music/', filter_by='.mp3')
 
 if __name__ == '__main__':
-    pass
-    #main()
+    main()
